7cde16f4d43b7fbbc4b4a21b1f506f3edacbbfb6
AdelaSantallaRuizDeCortazar <sanruiade@gmail.com>
2025-02-17 13:23:43 +0100

    formularios y acciones con blade y eloquent

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>Create product</h1>
+    <form method="POST" action="{{ route('products.update', ['product' => $product->id]) }}">
+        @csrf
+        @method('PUT')
+        <div class="form-row">
+            <label>Title</label>
+            <input class="form-control" type="text" name="title" value="{{ $product->title }}" required>
+        </div>
+        <div class="form-row">
+            <label>Description</label>
+            <input class="form-control" type="text" name="description" value="{{ $product->description }}" required>
+        </div>
+        <div class="form-row">
+            <label>Price</label>
+            <input class="form-control" type="number" name="price" min="1.00" step="0.01"
+                value="{{ $product->price }}" required>
+        </div>
+        <div class="form-row">
+            <label>Stock</label>
+            <input class="form-control" type="numer" name="stock" min="0" value="{{ $product->stock }}" required>
+        </div>
+        <div class="form-row">
+            <label>Status</label>
+            <select class="custom-select" name="status" required>
+                <option value="">Select...</option>
+                <option {{ $product->status == 'available' ? 'selected' : '' }} value="available">Available</option>
+                <option {{ $product->status == 'unavailable' ? 'selected' : '' }} value="unavailable">Unavailable</option>
+            </select>
+        </div>
+        <div class="form-row">
+            <button type="submit" class="btn btn-primary btn-lg">Edit product</button>
+        </div>
+    </form>
+@endsection
